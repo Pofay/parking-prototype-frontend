87ebf1c4dd53e6bdf483ed8e6305c1b4604d7927
Gian Carlo Gilos <gianCarlo.gilos@outlook.com>
2018-02-23 03:38:15 +0800

    Created AreaListComponent and added to Declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AreaListComponent } from './areas/area-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AreaListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/areas/area-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector : "pp-root",
+    templateUrl : "./area-list.component.html",
+})
+
+export class AreaListComponent {}
