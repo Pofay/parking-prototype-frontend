be69a102220e85cd67942a09b7f022f6d49f55d3
Gian Carlo Gilos <gianCarlo.gilos@outlook.com>
2018-02-23 04:49:09 +0800

    Can now render a list of Parking Areas

--- src/app/areas/area-list.component.ts
+++ src/app/areas/area-list.component.ts
@@ -8,4 +8,18 @@ import { Component } from "@angular/core";
 export class AreaListComponent 
 {
     pageTitle : string = "List of Parking Areas";
+    parking_areas: any[] = 
+    [
+        {
+            "name": "Canteen Area",
+            "id": 1,
+            "available_spots": 2
+        },
+        {
+            "name": "Main Area",
+            "id": 2,
+            "available_spots": 1
+        }
+    ];
+    
 }
